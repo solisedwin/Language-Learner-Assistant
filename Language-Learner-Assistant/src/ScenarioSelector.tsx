c77812e3b271d2ce4ec6d3f5d4e1dca78caa1356
solisedwin10 <solisedwin10@gmail.com>
2026-01-25 21:02:53 -0500

    Created an axios interceptor that sets up the baseurl and headers for every single axios request.  And for further modifications in the future such as setting up Auth Bearer tokens in the header. Made a new component for the Scenario Selector buttons.

---
+++ Language-Learner-Assistant/src/ScenarioSelector.tsx
@@ -0,0 +1,88 @@
+import Fab from '@mui/material/Fab';
+import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
+import TrainIcon from '@mui/icons-material/Train';
+import HotelIcon from '@mui/icons-material/Hotel';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import { useState } from 'react';
+import { ToggleButton } from '@mui/material';
+import type { RoleplayScenario } from '../backend/types';
+import {request} from './AxiosUtil';
+
+function ScenarioSelector() {
+    const [roleplayScenario, setRoleplayScenario] = useState<RoleplayScenario>('Supermarket');
+
+    const startRolePlay = () => {
+        request({
+            method: 'POST',
+            url: '/OpenAI'
+        }).then(response => {
+            console.log(response);
+        }).catch(error => {
+            console.log(error)
+        })
+    };
+
+    return (
+        <div>
+            Current Roleplay scenario: {roleplayScenario}
+
+            <ToggleButtonGroup
+                value={roleplayScenario}
+                onChange={(_, roleplayScenario) => setRoleplayScenario(roleplayScenario)}
+                exclusive
+                aria-label='Roleplay Buttons'
+            >
+
+                <Grid container spacing={3}>
+                    <Grid size={4}>
+                        <ToggleButton value='Supermarket'>
+                            <Fab
+                                variant='extended'
+                                color='primary'
+                                component='div'
+                            >
+                                <LocalGroceryStoreIcon sx={{ mr: 1 }} />
+                                Grocery Store
+                            </Fab>
+                        </ToggleButton>
+                    </Grid>
+
+                    <Grid size={4}>
+                        <ToggleButton value='TrainStation'>
+                            <Fab
+                                variant='extended'
+                                color='secondary'
+                                component='div'
+                            >
+                                <TrainIcon sx={{ mr: 1 }} />
+                                Train Station
+                            </Fab>
+                        </ToggleButton>
+                    </Grid>
+
+                    <Grid size={4}>
+                        <ToggleButton value='Hotel'>
+                            <Fab
+                                variant='extended'
+                                color='warning'
+                                component='div'
+                            >
+                                <HotelIcon sx={{ mr: 1 }} />
+                                Hotel
+                            </Fab>
+                        </ToggleButton>
+                    </Grid>
+
+                    <Grid size={12}>
+                        <Button variant="contained" color='success' onClick={() => startRolePlay()}> Start Roleplay </Button>
+                    </Grid>
+                </Grid>
+            </ToggleButtonGroup>
+
+        </div>
+    );
+}
+
+export default ScenarioSelector;
