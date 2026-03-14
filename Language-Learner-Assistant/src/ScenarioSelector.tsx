4b6c3cd6fb62703d52e3a637e80d0fd29fe6f240
solisedwin10 <solisedwin10@gmail.com>
2026-03-14 18:19:07 -0400

    Created new component to display the Roleplay option buttons on the Scenario Selector page

--- Language-Learner-Assistant/src/ScenarioSelector.tsx
+++ Language-Learner-Assistant/src/ScenarioSelector.tsx
@@ -1,28 +1,13 @@
 import LoadingIndicator from './LoadingIndicator.tsx';
 import { useLoading } from './hooks/useLoading.ts';
-import Fab, { type FabProps } from '@mui/material/Fab';
-import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
-import TrainIcon from '@mui/icons-material/Train';
-import HotelIcon from '@mui/icons-material/Hotel';
-import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import { useEffect, useRef, useState } from 'react';
-import { ToggleButton, type SvgIconTypeMap } from '@mui/material';
+import { useRef, useState } from 'react';
 import type { RoleplayScenario } from './../shared/types/RoleplayScenario.ts';
 import { startConversation } from './api/features/Conversation/Conversation.ts';
-import type { OverridableComponent } from '@mui/material/OverridableComponent';
+import RolePlayOptionButtons from './RolePlayOptionButtons.tsx';
 import Microphone from './Microphone.tsx';
 import Translation from './Translation.tsx';
 import type { AIConversationResponse } from '@shared/types/Conversation.ts';
-
-type MuiIconComponent = OverridableComponent<SvgIconTypeMap<{}, "svg">>;
-
-type RoleplayScenarioButtons = {
-    scenario: RoleplayScenario,
-    icon: MuiIconComponent,
-    color:  FabProps['color'],
-}
 
 type LanguageTexts = {
     germanText: string,
@@ -40,25 +25,10 @@ function ScenarioSelector() {
     const [audioSource, setAudioSource] = useState('');
 
     const AIAudioSpeechRef = useRef<HTMLAudioElement>(null)
-    const roleplayScenarioOptions : RoleplayScenarioButtons[]  = [
-        {
-            scenario: 'Supermarket',
-            icon: LocalGroceryStoreIcon,
-            color: 'primary'
-        },
-        {
-            scenario: 'TrainStation',
-            icon: TrainIcon,
-            color: 'secondary'
-        },
-        {
-            scenario: 'Hotel',
-            icon: HotelIcon,
-            color: 'warning'
-            
-        }];
-
-    const {isLoading, startRequest, stopRequest} = useLoading();
+  
+    const onRolePlaySelection = (rolePlay: RoleplayScenario) => {
+        setRoleplayScenario(rolePlay);
+    }
 
     const startRolePlay = async () => {
         setIsShowingRoleplayOptionButtons(false);
@@ -84,51 +54,27 @@ function ScenarioSelector() {
         }
     };
 
+    const {isLoading, startRequest, stopRequest} = useLoading();
+
     return (
         <div>
             Current Roleplay scenario: {roleplayScenario}
-            { isShowingRoleplayOptionButtons && 
-                <ToggleButtonGroup
-                    value={roleplayScenario}
-                    onChange={(_, roleplayScenario) => setRoleplayScenario(roleplayScenario)}
-                    exclusive
-                    aria-label='Roleplay Buttons'
-                >
-            
-                <Grid container spacing={3}>
-                    {
-                            roleplayScenarioOptions.map( ({scenario, icon : Icon, color}, _ ) => (
-                                <ToggleButton 
-                                    key={scenario}
-                                    value={scenario}
-                                >
-                                    <Fab
-                                        variant='extended'
-                                        color={color}
-                                        component='div'
-                                    >
-                                        <Icon sx={{ mr: 1 }} />
-                                        {scenario}
-                                    </Fab>
-                                </ToggleButton>
-                            ))
-                    }
-
-                    <Grid size={12}>
-                        <Button
-                            variant="contained"
-                            color='success'
-                            onClick={startRolePlay}>
-                                Start Roleplay
-                        </Button>
-                    </Grid>
-                    </Grid>
-
-                </ToggleButtonGroup>
+            { 
+                isShowingRoleplayOptionButtons 
+                    &&  
+                <RolePlayOptionButtons
+                    onRolePlaySelection = {onRolePlaySelection}
+                /> 
             }
+                
+            <Button
+                variant="contained"
+                color='success'
+                onClick={startRolePlay}>
+                Start Roleplay
+            </Button>
 
             {isLoading && <LoadingIndicator /> }
-
 
        {audioSource && (
             <audio
