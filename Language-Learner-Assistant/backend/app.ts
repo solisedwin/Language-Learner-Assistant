1a4917d33f2c3b44a77a2186b3b4d5e18e09e147
solisedwin10 <solisedwin10@gmail.com>
2026-03-18 22:27:48 -0400

    Added mongodb connection and installed mongoose package to project.

--- Language-Learner-Assistant/backend/app.ts
+++ Language-Learner-Assistant/backend/app.ts
@@ -3,10 +3,26 @@ import "./envconfig.ts";
 import express from "express";
 import cors from "cors";
 import ConversationRouter from "./routes/ConversationRoutes.ts";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
 app.use(helmet());
+
+// Database connection
+try {
+  async function connectDB() {
+    const mongoURI = process.env.MONGO_DB_URI;
+    if (!mongoURI) {
+      throw new Error("Mongo URL is not set !");
+    }
+    const conn = await mongoose.connect(mongoURI);
+    console.log(`~~~ Database connected: ${conn.connection.host}`);
+  }
+  connectDB();
+} catch (error) {
+  console.error("Error when connecting to MongoDb: ", error);
+}
 
 const corsOptions = {
   origin: "http://localhost:5173",
